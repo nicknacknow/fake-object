b4dfc4e5d629405599a27e0c4f130f00b32d8e9d
nicholas <nicknacknow@users.noreply.github.com>
2019-08-15 17:00:41 +0100

    Module used to create fake objects in Lua
    A module used to create fake objects in Lua (by using the newproxy function). Useful for keeping data and code clean.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # fake-object
- A module used to create fake objects in Lua (by using newproxy function). Useful for keeping data and code clean.
+
+This module can be required in your Lua code.
+It uses the newproxy function which is only available in earlier versions of Lua (I believe).
+Personally, I use Lua 5.1.4 however 5.1.5 supports this aswell.
+
+By requiring it, you call the new() function, which can take a string as an arguement for the name of this object.
+The fake object is returned from the function for you to use in your code.
+When trying to output/print the object, it'll just output the name given in the function.
+If you don't give a name, it'll automatically assume 'object' as its name.
+
+Example:
+
+local object = require("fake-object"):new("object-name")
+
+object.name = "hello!"
+
+print(object.name) >> "hello!"
+
+print(object) >> "object-name"
---
+++ fake-object.lua
@@ -0,0 +1,26 @@
+local magic = {} -- table which is returned when module is required
+
+function magic:new(name) -- function 'new' which will be in the returned table to be called
+	name = not name or type(name)~="string" and "object" or name -- if the name is invalid then call it "object", if it is, then leave it as it is
+
+	local new = newproxy(true) -- newproxy returns a userdata, and, if given the arg true, with a blank metadata.
+	local mt = getmetatable(new) -- gets the metatable of the userdata
+
+	local data = {} -- this is where data will be stored for the object
+
+	mt.__index = function(t,k) -- index metamethod is when you try and get a property (e.g. object.name). t is the metatable ( i believe ) and k is what was indexed (e.g. "name")
+		return rawget(data,k) -- attempts to get 'k' from the data table, if k doesn't exist in data it returns nil. rawget basically indexes data with k without calling the index metamethod
+	end
+
+	mt.__newindex = function(t,k,v) -- newindex metamethod is when you try and set a property (e.g. object.name = "Hi"). v is the value that k is being set with (e.g. "Hi")
+		rawset(data,k,v) -- attempts to set data[k]=v, basically meaning that k in the data table is now what v is. (e.g. object.name = "hi" will set "name" in data to "hi")
+	end
+
+	mt.__tostring = function() return name end -- when being outputted, instead of being a bunch of hex stuff (since its a userdata), it'll just output the given name.
+											   -- tostring metamethod is when the object is trying to be turned into string, e.g tostring(object) will call this metamethod.
+	
+	mt.__metatable = "metatable not available" -- if someone tries to get the objects metatable it'll return this string instead of the actual metatable
+											   -- this is used to prevent people who are venturing to tamper with the code.
+	return new
+
+end
